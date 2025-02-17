5277dd54b027ffee9f6c9eb0d2e5581dd3da5f9f
Lorena Salas <lorena23031990@gmail.com>
2025-02-16 21:40:14 -0400

    Base de Pagina Principal

---
+++ src/components/general/Footer.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { useStore } from "vuex";
+const store = useStore();
+</script>
+<template>
+  <footer class="py-3 footer">
+    <div class="container-fluid">
+      <div class="row align-items-center justify-content-lg-between">
+        <div class="mb-4 col-lg-6 mb-lg-0">
+          <div
+            class="text-sm text-center copyright text-muted"
+            :class="store.state.isRTL ? 'text-lg-end' : 'text-lg-start'"
+          >
+            ©
+            {{ new Date().getFullYear() }}, hecho con 
+            <i class="fa fa-heart"></i> por
+            <a
+              href="#"
+              class="font-weight-bold"
+              target="_blank"
+              >Lorena Salas</a
+            >. Reservados todos los derechos.
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <ul
+            class="nav nav-footer justify-content-center justify-content-lg-end"
+          >
+            <li class="nav-item">
+              <a
+                href="#"
+                class="nav-link text-muted"
+                target="_blank"
+                >Seguridad</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                href="#"
+                class="nav-link text-muted"
+                target="_blank"
+                >Privacidad</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                href="#"
+                class="nav-link text-muted"
+                target="_blank"
+                >Términos y Condiciones</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
